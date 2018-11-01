f7c5c12128fb89b4219bdde5d29f52036a63fc53
Duncan <duncan@lew.email>
2018-11-01 09:08:23 +0100

    almost all edge movements restricted

--- js/app.js
+++ js/app.js
@@ -5,47 +5,79 @@
 const incrementX = 101;
 const incrementY = 83;
 const startingX = 202;
-const startingY = 50 + (4*83);
+const startingY = 50 + (4 * 83);
 
-var Enemy = function(x, y) {
+var Enemy = function (x, y) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = 10;
 };
 
-Enemy.prototype.update = function(dt) {
-    this.x += this.speed*dt;
+Enemy.prototype.update = function (dt) {
+    this.x += this.speed * dt;
     this.y = this.y;
     //HANDLE COLLISION WITH PLAYER
 };
 
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-var Player = function() {
+var Player = function () {
     this.sprite = 'images/char-boy.png';
     this.x = startingX;
     this.y = startingY;
     this.deltaX = 0;
     this.deltaY = 0;
 }
-Player.prototype.update = function(deltaX, deltaY) {
+Player.prototype.update = function (deltaX, deltaY) {
     this.x += this.deltaX;
     this.y += this.deltaY;
     this.deltaX = 0;
     this.deltaY = 0;
+    //console.log("Player x: "+ this.x);
+    //console.log("Player y: "+ this.y);
 };
 
-Player.prototype.render = function() {
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function(inputKeyString) {
-    switch(inputKeyString) {
+Player.prototype.reset = function () {
+    this.x = startingX;
+    this.y = startingY;
+}
+
+Player.prototype.canPlayerMoveLeft = function () {
+    if (this.x < incrementX) {
+        return false;
+    } 
+    return true;
+}
+
+Player.prototype.canPlayerMoveRight = function () {
+    if (this.x + incrementX >= canvas.width) {
+        return false;
+    }
+    return true;
+}
+
+Player.prototype.canPlayerMoveInDown = function () {
+    if (this.y + incrementY > 382) {
+        return false;
+    }
+    return true;
+}
+
+Player.prototype.handleInput = function (inputKeyString) {
+    console.log("Player x: "+ this.x);
+    console.log("Player y: "+ this.y);
+    switch (inputKeyString) {
         case 'left':
-            this.deltaX = -incrementX;
+            if (this.canPlayerMoveLeft()) {
+                this.deltaX = -incrementX;
+            }
             break;
 
         case 'up':
@@ -53,21 +85,27 @@ Player.prototype.handleInput = function(inputKeyString) {
             break;
 
         case 'right':
-            this.deltaX = incrementX;
+            if (this.canPlayerMoveRight()) {
+                this.deltaX = incrementX;
+            }
             break;
 
         case 'down':
+        if (this.canPlayerMoveInDown()) {
             this.deltaY = incrementY;
+        }
             break;
     }
+    console.log(inputKeyString);
 };
 
 var player = new Player();
-var allEnemies = [new Enemy(0, 50), 
-    new Enemy(0, 50+83), 
-    new Enemy(0, 50+2*83)];
+var allEnemies = [new Enemy(-101, 50),
+    new Enemy(0, 50 + 83),
+    new Enemy(0, 50 + 2 * 83)
+];
 
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
