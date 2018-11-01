2940a27ba0f1c1dd244f703dfd98505763e03aeb
Duncan <duncan@lew.email>
2018-11-01 11:13:10 +0100

    comments added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
-// Constants that are used for the
-// starting location and movements of the player.
-// Movement of the player is performed in
-// increments in either the X or Y direction.
+// Constants that defined for the 
+// locations, boundaries and other properties of the game
 const incrementX = 101;
 const incrementY = 83;
 const startLocationXPlayer = 202;
@@ -17,17 +15,21 @@ const numberOfLanes = 3
 var player;
 var allEnemies;
 
+// Get the lane number in which the enemy will appear
+// The lane numbers that can be chosen are 0, 1 and 2. 
 function getLaneNumber() {
-    // The lane numbers that can be chosen are 
-    // 0, 1 and 2. 
     return Math.floor(Math.random() * numberOfLanes)
 }
 
+// Initialize the player and enemy objects
+// Used for starting/resetting the game
 function initializeObjects() {
     player = new Player();
     allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 }
 
+// Return a random number between minSpeed and maxSpeed.
+// This is used for asigning a random speed to an Enemy object
 function getRandomSpeed() {
     return Math.floor(Math.random() * (maxSpeed - minSpeed + 1)) + minSpeed;
 }
@@ -39,13 +41,18 @@ var Enemy = function () {
     this.speed = getRandomSpeed();
 };
 
+// If the enemy object moves off screen, its location and
+// its speed will get regenerated
 Enemy.prototype.resetEnemyLocationIfOffScreen = function () {
     if (this.x >= canvas.width) {
         this.x = startLocationXEnemy;
         this.y = boundaryWater + getLaneNumber() * incrementY;
+        this.speed = getRandomSpeed();
     }
 }
 
+// If the enemy object collides with the player object,
+// the game will get resetted.
 Enemy.prototype.resetGameIfCollisionDetected = function () {
     if (Math.abs(this.y - player.y) < incrementY) {
         if (Math.abs(this.x - player.x) <= collisionSensitivity * incrementX) {
@@ -54,6 +61,9 @@ Enemy.prototype.resetGameIfCollisionDetected = function () {
     }
 };
 
+// When the enemy position is updated, check whether
+// a collision has occured with the player object
+// and whether the enemy object is off screen
 Enemy.prototype.update = function (dt) {
     this.x += this.speed * dt;
     this.y = this.y;
@@ -84,11 +94,15 @@ Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Reset the location of the player object.
+// This happens only when the player has won.
 Player.prototype.reset = function () {
     this.x = startLocationXPlayer;
     this.y = startLocationYPlayer;
 }
 
+// Check whether the player object can move left
+// This is done to prevent player from moving off screen
 Player.prototype.canPlayerMoveLeft = function () {
     if (this.x < incrementX) {
         return false;
@@ -96,6 +110,8 @@ Player.prototype.canPlayerMoveLeft = function () {
     return true;
 }
 
+// Check whether the player object can move right
+// This is done to prevent player from moving off screen
 Player.prototype.canPlayerMoveRight = function () {
     if (this.x + incrementX >= canvas.width) {
         return false;
@@ -103,6 +119,8 @@ Player.prototype.canPlayerMoveRight = function () {
     return true;
 }
 
+// Check whether the player object can move down
+// This is done to prevent player from moving off screen
 Player.prototype.canPlayerMoveInDown = function () {
     if (this.y + incrementY > boundaryBottomBoard) {
         return false;
@@ -110,6 +128,8 @@ Player.prototype.canPlayerMoveInDown = function () {
     return true;
 }
 
+// Check whether the player object can move up
+// This is done to prevent player from moving off screen
 Player.prototype.resetLocationIfPlayerWins = function () {
     if (this.y < boundaryWater) {
         this.reset();
